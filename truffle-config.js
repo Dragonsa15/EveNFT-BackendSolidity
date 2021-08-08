d646b1a81fdf0f0bf82ed23ae1cad507c3a23efe
Sai Rithwik M <rithwiksaim@gmail.com>
2021-08-08 23:21:24 +0530

    Added support for Truffle bounty

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,28 @@
 const path = require("path");
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "../canvas/client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
-    }
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+  mocha: {
+    timeout: 100000
   },
   compilers: {
     solc: {
